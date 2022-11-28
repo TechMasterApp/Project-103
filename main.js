af217759c5ce0bfb8163fc8e69ea6d01b5fe4595
TechMasterApp <84641823+TechMasterApp@users.noreply.github.com>
2022-11-27 21:08:25 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format:"png",
+    png_quality: 90
+})
+
+Webcam.attach("#webcam")
+
+function capture() {
+    Webcam.snap((data_uri) => {
+        document.getElementById("snapshot").innerHTML = "<img id ='snapimage' src=" + data_uri + ">"
+    })
+}
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/umRMyPIQd/model.json", modelLoaded)
+
+function modelLoaded() {
+    console.log("Model is OK")
+}
+
+function identify() {
+    img = document.getElementById("snapimage")
+    classifier.classify(img, gotResult)
+}
+
+function gotResult(error, result) {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log(result)
+    }
+}
